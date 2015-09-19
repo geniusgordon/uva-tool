60b6b0dbacda24f2ab8d20f549c9a333f894dcd6
Gordon <geniusgordon@gmail.com>
2015-09-19 23:47:54 +0800

    write problem

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from login import login
 from submit import submit as _submit
 from uhunt import ac_count as _ac_count
+from problem import write as _write
 from problem import download_pdf as _download_pdf
 
 def submit(args):
@@ -21,6 +22,9 @@ def download_pdf(args):
     except Exception as e:
         print e
 
+def write(args):
+    _write(args.problem)
+
 parser = argparse.ArgumentParser(description='A uva command line tool')
 subparser = parser.add_subparsers()
 
@@ -36,6 +40,10 @@ p.set_defaults(func=ac_count)
 p = subparser.add_parser('pdf', help='download problem pdf')
 p.add_argument('problem', help='problem id')
 p.set_defaults(func=download_pdf)
+
+p = subparser.add_parser('write', help='write problem')
+p.add_argument('problem', help='problem id')
+p.set_defaults(func=write)
 
 args = parser.parse_args()
 
--- problem.py
+++ problem.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+import subprocess
 from config import config
 
 def cli_progress(filename, progress, bar_length=20):
@@ -33,6 +34,31 @@ def download_pdf(pid):
             cnt += 1
     print ''
 
+def write(pid):
+    filename = 'downloads/%s.pdf' % pid
+    if not os.path.exists(filename):
+        download_pdf(pid)
+    with open(os.devnull, 'w') as f:
+        subprocess.call(['xdg-open', filename], stderr=f)
+    filename = 'codes/%s.cpp' % pid
+    includes = config.get('problem', 'includes').split(',')
+    template = 'int main()\n{\n\treturn 0;\n}\n\n'
+    if not os.path.exists('codes/'):
+        os.makedirs('codes/')
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            for include in includes:
+                f.write('#include <%s>\n' % include.strip())
+            f.write('using namespace std;\n\n')
+            f.write(template)
+    os.system('vim ' + filename)
+    os.system('vim input')
+    ret = os.system('g++ -g ' + filename)
+    if ret == 0:
+        os.system('./a.out < input > output')
+        os.system('cat output')
+
 if __name__ == '__main__':
-    download_pdf(11234)
+    # download_pdf(11234)
+    write(123)
 
